74b92e92b104d83a774409e47006b19996b09214
k-olesinski <k.olesinski@wp.pl>
2016-01-21 17:08:35 +0100

    calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,32 +1,35 @@
 /**
  * Created by kristo on 21.01.16.
  */
-function Calendar() {
-    var $board;
-    var months = [
-        {month: 'STYCZEŃ',numberOfDays: 31},
-        {month: 'LUTY', numberOfDays: 28}, 'MARZEC', 'KWIECIEŃ', 'MAJ', 'CZERWIEC',
-                  'LIPIEC', 'SIERPIEŃ', 'WRZESIEŃ', 'PAŹDZIERNIK', 'LISTOPAD', 'GRUDZIEŃ'];
-    var days = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'];
+$(function() {
+    function Calendar() {
+        var $board;
+        var months = [
+            {month: 'STYCZEŃ',numberOfDays: 31},
+            {month: 'LUTY', numberOfDays: 28}, 'MARZEC', 'KWIECIEŃ', 'MAJ', 'CZERWIEC',
+            'LIPIEC', 'SIERPIEŃ', 'WRZESIEŃ', 'PAŹDZIERNIK', 'LISTOPAD', 'GRUDZIEŃ'];
+        var days = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'];
 
 
-    function createTable() {
-        var numberOfWeekDays = 7;
-        var $table = $('<table>');
-        for (var y = 0; y < months[0].numberOfDays; y++) {
-            var $row = $('<tr>');
-            for (var x = 0; x < numberOfWeekDays; x++) {
-                $row.append($('<td>').attr('x', x).attr('y', y));
+        function createTable() {
+            var numberOfWeekDays = 7;
+            var $table = $('<table>');
+            for (var y = 0; y < months[0].numberOfDays; y++) {
+                var $row = $('<tr>');
+                for (var x = 0; x < numberOfWeekDays; x++) {
+                    $row.append($('<td>').attr('x', x).attr('y', y));
+                }
+                $table.append($row);
             }
-            $table.append($row);
+            return $table;
         }
-        return $table;
-    }
 
-    $board = $('.cal');
-    $board.append(createTable());
-}
-Calendar();
+        $board = $('.cal');
+        $board.append(createTable());
+    }
+    Calendar();
+});
+
 
 //function daysInMonth(month,year) {
 //    return new Date(year, month, 0).getDate();
