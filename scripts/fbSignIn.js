18002dbc74b3fdd212014fe7997e3dc63260d0e3
Jakub Klos <jakub.klos91@gmail.com>
2016-01-22 14:09:42 +0100

    zmiana w kodzie FB login

--- scripts/fbSignIn.js
+++ scripts/fbSignIn.js
@@ -77,8 +77,6 @@ $(document).ready(function() {
         console.log('Welcome!  Fetching your information.... ');
         FB.api('/me', function(response) {
             console.log('Successful login for: ' + response.name);
-            document.getElementById('status').innerHTML =
-                'Thanks for logging in, ' + response.name + '!';
         });
     }
 
