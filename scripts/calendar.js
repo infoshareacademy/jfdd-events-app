5585c9ed471c84e426ddf55b2129bff345ac853e
k-olesinski <k.olesinski@wp.pl>
2016-01-22 11:16:25 +0100

    calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -4,19 +4,22 @@
 $(function() {
     function Calendar() {
         var $board;
+        var daysInMonth = function (month,year) {
+            return new Date(year, month, 0).getDate();
+        };
         var months = [
-            {month: 'STYCZEŃ',numberOfDays: 31},
-            {month: 'LUTY', numberOfDays: 28}, 'MARZEC', 'KWIECIEŃ', 'MAJ', 'CZERWIEC',
+            'STYCZEŃ', 'LUTY', 'MARZEC', 'KWIECIEŃ', 'MAJ', 'CZERWIEC',
             'LIPIEC', 'SIERPIEŃ', 'WRZESIEŃ', 'PAŹDZIERNIK', 'LISTOPAD', 'GRUDZIEŃ'];
         var days = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'];
 
 
         function createTable() {
-            var numberOfWeekDays = 7;
+            var numberOfDays = 7;
+            var numberOfWeeks = daysInMonth(months.indexOf(months[0]),2015)%7;
             var $table = $('<table>');
-            for (var y = 0; y < months[0].numberOfDays; y++) {
+            for (var y = 0; y < numberOfWeeks; y++) {
                 var $row = $('<tr>');
-                for (var x = 0; x < numberOfWeekDays; x++) {
+                for (var x = 0; x < numberOfDays; x++) {
                     $row.append($('<td>').attr('x', x).attr('y', y));
                 }
                 $table.append($row);
@@ -29,8 +32,3 @@ $(function() {
     }
     Calendar();
 });
-
-
-//function daysInMonth(month,year) {
-//    return new Date(year, month, 0).getDate();
-//}
