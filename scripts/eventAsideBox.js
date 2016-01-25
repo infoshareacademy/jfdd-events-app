b04062c652aa036edde1c16ab5e5e43d03a6cac3
mac <12345>
2016-01-25 19:14:10 +0100

    details kolo kalendarza/ animacja

--- scripts/eventAsideBox.js
+++ scripts/eventAsideBox.js
@@ -1,12 +1,14 @@
 /**
- * Created by klos on 22.01.2016.
+ * Created by klos/michal on 22.01.2016.
  */
 $(function() {
     $("#accordion").accordion();
 
-$("button").click(function(){
-    $(".asideBoxSection").fadeIn(1000);
-    $("#calendar").addClass("col-sm-8");
-});
+    $(".asideBoxSection").hide();
+
+    $("button").click(function(){
+        $(".asideBoxSection").fadeIn(1000);
+        $("#calendar").addClass("col-sm-8");
+    });
 
 });
