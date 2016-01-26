f4f2a23fa5b9dfc82c4409bc15a44c34b10795aa
Monika Kałużyńska <m.kaluzynska@wp.pl>
2016-01-26 16:48:05 +0100

    logowanie przez Google+/wylogowanie

--- scripts/googleSignIn.js
+++ scripts/googleSignIn.js
@@ -15,3 +15,16 @@ $(document).ready(function() {
         });
     }
 });
+
+//gapi.load('auth2', function() {
+//    auth2 = gapi.auth2.init({
+//        client_id: 'CLIENT_ID.apps.googleusercontent.com',
+//        fetch_basic_profile: false,
+//        scope: 'profile'
+//    });
+//
+//    // Sign the user in, and then retrieve their ID.
+//    auth2.signIn().then(function() {
+//        console.log(auth2.currentUser.get().getId());
+//    });
+//});
