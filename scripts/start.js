1f9314ff89e86e772f391af79e48be2a60a6256a
kubusklos <jakub.klos91@gmail.com>
2016-01-27 22:26:54 +0100

    Revert "Feature/kuba/logging"

--- scripts/start.js
+++ scripts/start.js
@@ -1,44 +1,56 @@
 /**
  * Created by klos on 26.01.2016.
  */
+$(document).ready(function () {
+
+    var blurEffect = function () {
+        $('.logInButton').mouseenter(function() {
+            $('.backgroundImage').stop().animate(
+                {blurRadius: 3},
+                {
+                    duration: 1000,
+                    easing: 'easeInOutCubic',
+
+                    step: function () {
+                        $('.backgroundImage').css({
+                            "-webkit-filter": "blur(" + this.blurRadius + "px)",
+                            "filter": "blur(" + this.blurRadius + "px)"
+                        });
+                    }
+                }
+            );
+        });
+    };
+    blurEffect();
+
+
+    var blurClose = function () {
+        $('.logInButton').mouseleave(function() {
+            $('.backgroundImage').stop().animate(
+                {blurRadius: 0},
+                {
+                    duration: 1000,
+                    easing: 'easeInOutCubic',
+
+                    step: function () {
+                        $('.backgroundImage').css({
+                            "-webkit-filter": "blur(" + this.blurRadius + "px)",
+                            "filter": "blur(" + this.blurRadius + "px)"
+                        });
+                    }
+                }
+            );
+        });
+    };
+    blurClose();
+
+});
+
 
 $('.logInButton, .logInMenu').click(function() {
-    $('.popUp').show();
-    $('.mainContent').hide();
-    $('.backgroundImage').stop().animate(
-        {blurRadius: 3},
-        {
-            duration: 1000,
-            easing: 'easeInOutCubic',
-
-            step: function () {
-                $('.backgroundImage').css({
-                    "-webkit-filter": "blur(" + this.blurRadius + "px)",
-                    "filter": "blur(" + this.blurRadius + "px)"
-                });
-            }
-        }
-    );
+    $('.popUp').show("scale","swing", 1000);
 });
-
-
 
 $(".closingCross").click(function() {
-    $(".popUp").hide();
-    $('.mainContent').show();
-    $('.backgroundImage').stop().animate(
-        {blurRadius: 0},
-        {
-            duration: 1000,
-            easing: 'easeInOutCubic',
-
-            step: function () {
-                $('.backgroundImage').css({
-                    "-webkit-filter": "blur(" + this.blurRadius + "px)",
-                    "filter": "blur(" + this.blurRadius + "px)"
-                });
-            }
-        }
-    );
+    $(".popUp").hide("scale","swing", 1000);
 });
-
