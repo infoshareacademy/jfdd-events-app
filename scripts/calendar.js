24d574d334f8aea1e97bd864c68327214cf99074
mac <12345>
2016-01-30 16:34:09 +0100

    poprawki

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -12,6 +12,18 @@ $(document).ready(function() {
             center: 'title',
             right: 'month,agendaWeek,agendaDay'
         },
+        eventLimit: true, // for all non-agenda views
+        views: {
+            agenda: {
+                eventLimit: 1 // adjust to 6 only for agendaWeek/agendaDay
+            }
+        },
+        lang: 'pl',
+        eventBackgroundColor:(255,0,0),
+        aspectRatio:2,
+        contentHeight: 450,
+        //height: 620,
+
         eventRender: function(event, element) {
             $(element).attr('title', event.ownProps.url);
             $(element).on('click',(function () {
@@ -28,29 +40,20 @@ $(document).ready(function() {
             $(".collapse-1, .collapse-2, .collapse-3, " +
                 ".collapsed-group-item-1, .collapsed-group-item-2, .collapsed-group-item-3").empty();
             var todayEvents = events.filter(function (event) {
-               return $.fullCalendar.moment(event.start).format('YYYY-MM-DD') === date.format();
+                return $.fullCalendar.moment(event.start).format('YYYY-MM-DD') === date.format();
             });
             console.log(todayEvents);
 
-                todayEvents.forEach(function (event, index) {
-                    $(".asideBoxSection").fadeIn(1500);
-                    $("#calendar").addClass("col-sm-8");
-                    $(".collapsed-group-item-"+(index+1)).text(event.ownProps.eventName);
-                    $('.collapse-'+(index+1)).text(event.ownProps.description);
-                    $('button').css("display","block");
-                })
+            todayEvents.forEach(function (event, index) {
+                $(".asideBoxSection").fadeIn(1500);
+                $("#calendar").addClass("col-sm-8");
+                $(".collapsed-group-item-"+(index+1)).text(event.ownProps.eventName);
+                $('.collapse-'+(index+1)).text(event.ownProps.description);
+                $('button').css("display","block");
+            })
 
         },
-        eventLimit: true, // for all non-agenda views
-        views: {
-            agenda: {
-                eventLimit: 1 // adjust to 6 only for agendaWeek/agendaDay
-            }
-        },
-        lang: 'pl',
-        aspectRatio:2,
-        contentHeight: 450,
-        //height: 620,
+
 
         events: function(start, end, timezone, callback) {
             $.ajax({
