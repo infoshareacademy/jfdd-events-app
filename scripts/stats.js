650643893eb96a68a16590687c62b32d82229fb4
k-olesinski <k.olesinski@wp.pl>
2016-02-12 09:53:27 +0100

    list of favourites

--- scripts/stats.js
+++ scripts/stats.js
@@ -4,13 +4,11 @@
 $('.js-update-stats').on('click', function () {
     var dataOfLocalStorage = JSON.parse(localStorage.getItem('log')) || [{'value':'brak wybranych wydarzeń'}];
     var dataForDisplay = dataOfLocalStorage
-        .sort(function (a, b) {
-            return a.value > b.value ? 1 : -1;
-        })
         .reduce(function (a, b) {
             if (a.length > 0 && a[a.length - 1].eventName === b.value) {
                 if (b.type === 'addToFav') {
                     a[a.length - 1].occurrences += 10;
+                    a[a.length - 1].kind = 'addToFav';
                 }
                 if (b.type === 'show') {
                     a[a.length - 1].occurrences += 1;
@@ -22,11 +20,16 @@ $('.js-update-stats').on('click', function () {
                     occurrences: 1
                 }]);
             }
-        }, []);
+        }, [])
+        .sort(function (a, b) {
+            return a.occurrences < b.occurrences ? 1 : -1;
+        });
     $('.statsHeading').text('Statystyki dotyczące wydarzeń');
     $('.js-stats').empty();
     dataForDisplay.forEach(function (item) {
-        $('.js-stats').append($('<li>').append(item.eventName + ' : ' + item.occurrences));
+        $('.js-stats').append(((item.kind === 'addToFav') ?  $('<li class="list-group-item" style="color: red">') :
+            $('<li class="list-group-item">')).append($('<span class="badge">')
+            .append(item.occurrences)).append(item.eventName));
+        console.log(item.kind);
     });
-
 });
